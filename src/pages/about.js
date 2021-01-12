e5cd38b6cdb79b8bb8367be3b68f5c8480e672f5
Ian Goodnight <goodnight.ian@gmail.com>
2021-01-12 16:58:00 -0500

    everything but contact page and landing page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,92 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 import Layout from '../components/layout.js';
 import Metadata from '../components/metadata';
+import aboutStyles from './about.module.scss';
 
 const About = () => {
+  const data = useStaticQuery(
+    graphql`
+      query About {
+        streetView: file(relativePath: { eq: "images/street-view.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 1168) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        naturalLogo: file(relativePath: { eq: "images/nat-logo.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 200) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `
+  );
+
   return (
     <Layout>
       <Metadata title="About Natural Essentials Inc." />
       <div>
-        <h1>About me</h1>
-        <p>Let&apos;s give them something to talk about.</p>
+        <h1>
+          A Proven Leader in Specialty Ingredient Distribution and OTC Drug
+          Manufacturing
+        </h1>
+        <p>
+          Natural Essentials Inc.® is one of the nation&apos;s fastest-growing
+          privately-held specialty ingredient distributors and OTC drug
+          manufacturers, serving customers in retail and institutions throughout
+          North America for more than 20 years. Natural Essentials Inc.®&apos;s
+          success has revolved around our commitment to providing fantastic
+          service, quality, and cost while building long-term, mutually
+          beneficial relationships with our customers across multiple channels,
+          including retailers, consumer products companies, and private brand
+          manufacturers. We fully understand the critical role we play for each
+          of our customers&apos; success and help create competitive solutions
+          for every one of our customers&apos; needs.
+        </p>
+        <p>
+          Industry-leading commitment to low-cost supply and vast regulatory
+          experience uniquely positions Natural Essentials Inc.® to quickly meet
+          and exceed our customers&apos; demands. With three manufacturing and
+          distribution facilities totaling over 300,000 square feet, we process
+          and ship thousands of orders daily, representing one of the
+          fastest-growing, privately-held manufacturers/distributors in North
+          America.
+        </p>
+        <h2>Our approach:</h2>
+        <p>
+          Natural Essentials Inc.® understands consumer behavior and competitive
+          hurdles, enabling us to tailor solutions and product offerings to meet
+          today&apos;s consumers&apos; unique needs. We build strategic
+          partnerships with our suppliers and customers to ensure a seamless
+          process on every project. Our vertically integrated approach, starting
+          with global farm-direct relationships, allows us to ensure consistent
+          supply. That, coupled with the fact that we operate our own private
+          manufacturing facility, empowers us to meet some of the most
+          aggressive deadlines with ease.
+        </p>
+        <p>
+          Our dedication and commitment to sourcing and producing superior
+          products at an extraordinary value while providing on-time and
+          consistent service has been recognized by our customers, allowing us
+          to become a global force in recent years.
+        </p>
+        <div>
+          <Img
+            fluid={data.streetView.childImageSharp.fluid}
+            alt="Natural Essentials Inc.®, street-view"
+          />
+        </div>
+        <div className={aboutStyles.logo}>
+          <Img
+            fluid={data.naturalLogo.childImageSharp.fluid}
+            alt="Natural Essentials Inc.®"
+          />
+        </div>
       </div>
     </Layout>
   );
--- src/pages/co-packing.js
+++ src/pages/co-packing.js
@@ -18,6 +18,57 @@ const CoPacking = () => {
             }
           }
         }
+        tubeFilling: file(relativePath: { eq: "images/tube-filling.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 718) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        bottleFilling: file(relativePath: { eq: "images/bottle-filler.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 718) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        jarFilling: file(relativePath: { eq: "images/jar-filling.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 718) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        samplePackets: file(relativePath: { eq: "images/sample-packet.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 575) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        secondaryPackaging: file(
+          relativePath: { eq: "images/secondary-packaging.jpg" }
+        ) {
+          childImageSharp {
+            fluid(maxWidth: 175) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        blisterBurst: file(relativePath: { eq: "images/burst.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 175) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        halo: file(relativePath: { eq: "images/halo.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 175) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
       }
     `
   );
@@ -31,30 +82,31 @@ const CoPacking = () => {
         <h1>Co-packing / Private Label Manufacturing Services</h1>
         <p>
           We currently offer a wide range of co-packing and private label
-          manufacturing services including liquid filling, blister packing,
-          kitting, specialty compounding, and custom formulations to name a few.
-          It is important to note that our service offerings are constantly
-          evolving.
+          manufacturing services, including liquid filling, blister packing,
+          kitting, specialty compounding, and custom formulations, to name a
+          few. It is important to note that our service offerings are
+          continually evolving.
         </p>
         <div>
           <h2 className={coPackStyles.sectionTitle}>Core Service Offerings:</h2>
         </div>
         <div>
-          <div>
-            <h3>Lip Balm Contract Filling</h3>
+          <div className={coPackStyles.article}>
+            <h3 className={coPackStyles.competencies}>
+              Lip Balm Contract Filling
+            </h3>
             <p className={coPackStyles.explainer}>
               Natural Essentials Inc.® currently has a lip balm stick filling
               capacity of over 50 million units per year and growing. Top that
-              with a capacity to fill 70 million lip balm jars annually and
+              with an ability to produce 70 million lip balm jars annually and
               millions of lip gloss tubes, it is easy to see why we are a go-to
-              source for lip balm manufacturing. We currently manyfacture small
+              source for lip balm manufacturing. We currently manufacture small
               lip balm runs starting at only 5,000 units and have many customers
-              that run 1 million or more per run. In addition to our custom lip
-              balm filling, we currently stock over 20 SKU&apos;s of
-              &ldquo;stock lip balm&rdquo; in clear, white, and black sticks
-              that are in stock ready to be labeled for promotional customers.
-              Whatever your needs, Natural Essentials Inc.® has the capacity to
-              fill them quickly and economically.
+              ordering 1 million or more per run. In addition to our custom lip
+              balm filling, we currently stock over 20 SKU&apos;s of &quot;stock
+              lip balm&quot; in clear, white, and black sticks in stock, ready
+              to be labeled for promotional customers. Whatever your needs,
+              Natural Essentials Inc.® can fill them quickly and economically.
             </p>
             <div className={coPackStyles.demoWide}>
               <Img
@@ -63,10 +115,123 @@ const CoPacking = () => {
               />
             </div>
           </div>
-          <div>
-            <h3>Tube Contract Filling</h3>
-            <p className={coPackStyles.explainer}>Lorem Ipsum yadda yadda</p>
-            <div></div>
+          <div className={coPackStyles.article}>
+            <h3 className={coPackStyles.competencies}>Tube Contract Filling</h3>
+            <p className={coPackStyles.explainer}>
+              Natural Essentials Inc.® currently fills squeezable plastic tubes
+              ranging in size from .25 oz. up to 8 oz. We currently operate four
+              tube-fillers, including a 2-head ultrasonic sealer for those tough
+              jobs that require tube sealing through heavy ointments or balms.
+              Our current capacity for filling plastic tubes is roughly 250,000
+              units per day, depending on the tube size and product consistency.
+              Minimum runs on tube-filling start at only 10,000 units.
+            </p>
+            <div className={coPackStyles.demoWide}>
+              <Img
+                fluid={data.tubeFilling.childImageSharp.fluid}
+                alt="Tube filling"
+              />
+            </div>
+          </div>
+          <div className={coPackStyles.article}>
+            <h3 className={coPackStyles.competencies}>Bottle Filling</h3>
+            <p className={coPackStyles.explainer}>
+              Natural Essentials Inc.® currently has a bottle filling capacity
+              of well over 100 million units per year and growing. When it comes
+              to bottle capability to fill bottle sizes ranging from 3 mL up to
+              gallon jugs and everything in between. However, what makes us
+              stand out is our ability to offer product filling ranging from
+              water thin, foaming products to heavy ointments and creams.
+              Whether you&apos;re looking for a hot or cold pour, at Natural
+              Essentials Inc.®, we have a range of equipment for almost any
+              need.
+            </p>
+            <div className={coPackStyles.demoWide}>
+              <Img
+                fluid={data.bottleFilling.childImageSharp.fluid}
+                alt="Bottle filling"
+              />
+            </div>
+          </div>
+          <div className={coPackStyles.article}>
+            <h3 className={coPackStyles.competencies}>Jar Filling</h3>
+            <p className={coPackStyles.explainer}>
+              Jar filling can be more complicated than people think. Often,
+              products sold in jars are thick ointments and creams requiring
+              heat, large heads, and specialized equipment to efficiently fill
+              the product. We currently have a capacity of well over 100 million
+              units per year and growing. When it comes to jar filling, we can
+              fill jars ranging from 3 mL up to 1-gallon pails and everything in
+              between. Furthermore, most of our filling lines come outfitted
+              with jacketed tanks, which allows us to facilitate hot pour
+              products with ease. In addition to this, we have multiple
+              induction sealers allowing us to fill jars and seal them inline
+              without added cost. Along with filling simple products, we have
+              also worked on many projects that require multiple pours, such as
+              salt and sugar scrubs. Regardless of your needs, chances are
+              Natural Essentials Inc.® can offer a solution that is both
+              economical and timely.
+            </p>
+            <div className={coPackStyles.demoWide}>
+              <Img
+                fluid={data.jarFilling.childImageSharp.fluid}
+                alt="Jar filling"
+              />
+            </div>
+          </div>
+          <div className={coPackStyles.last}>
+            <div className={coPackStyles.article}>
+              <h3 className={coPackStyles.competencies}>
+                Sample Packet Filling / High-speed Form-fill Seal
+              </h3>
+              <p className={coPackStyles.explainer}>
+                Natural Essentials Inc.® currently operates multiple vertical
+                form-fill-seal machines allowing us to be highly competitive at
+                filling sample packets and other flexible packagings. With a
+                capacity of 400,000 units per day and run sizes starting at just
+                50,000, Natural Essentials Inc.® can satisfy customers of all
+                sizes.
+              </p>
+              <div className={coPackStyles.demoWide}>
+                <Img
+                  fluid={data.samplePackets.childImageSharp.fluid}
+                  alt="Sample packet filling"
+                />
+              </div>
+            </div>
+            <div className={coPackStyles.article}>
+              <h3 className={coPackStyles.competencies}>
+                Secondary Packaging (High-speed cartoning, blistering, shrink
+                wrapping, and kitting)
+              </h3>
+              <p className={coPackStyles.explainer}>
+                Natural Essentials Inc.® currently operates 5 Alloyd rotary
+                blister machines giving us a capacity of almost 300,000 units
+                per day. In addition to this, we also offer countless other
+                secondary packaging services such as high-speed cartoning,
+                shrink wrapping, display packing, and more, all under one roof.
+                At Natural Essentials Inc.®, we never outsource secondary
+                packaging, so you can rest assured your entire project comes
+                together seamlessly the first time, every time.
+              </p>
+              <div className={coPackStyles.gallery}>
+                <Img
+                  className={coPackStyles.galleryImage}
+                  fluid={data.blisterBurst.childImageSharp.fluid}
+                  alt="BLister packaging"
+                />
+                <Img
+                  className={coPackStyles.galleryImage}
+                  fluid={data.secondaryPackaging.childImageSharp.fluid}
+                  alt="Secondary packaging (high-speed cartoning, blistering, shrink wrapping, and kitting)"
+                />
+                <Img
+                  className={coPackStyles.galleryImage}
+                  fluid={data.halo.childImageSharp.fluid}
+                  alt="Secondary packaging"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
